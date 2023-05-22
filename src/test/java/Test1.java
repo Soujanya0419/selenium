4cd5ce896fc88e6583a8202788f089dd7bd03c7c
Soujanya0419 <soujanya.ram8@gmail.com>
2023-05-23 04:43:07 +0530

    ci

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,13 +1,21 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.*;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Test1 {
 
     @Test
-    public void test() throws InterruptedException {
+    public void test() throws InterruptedException, MalformedURLException {
         System.out.println("Successful test");
-        WebDriver driver=new ChromeDriver();
+         String hub="http://localhost:4444";
+        ChromeOptions options=new ChromeOptions();
+         String standalone_node="http://selenium__standalone-chrome:4444/wd/hub/";
+        WebDriver driver=new RemoteWebDriver(new URL(hub),options);
         driver.get("https://www.google.com");
         Thread.sleep(2000);
         driver.close();
